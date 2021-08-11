bf397b35b71f149295b851ff5ac29a55ee97f7b4
Burak Adıgüzel <adiguzelburak@icloud.com>
2021-08-11 17:22:50 +0300

    products added.

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,43 +1,61 @@
+/* eslint-disable react/style-prop-object */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import "../Style/products.scss";
 import burgerIcon from "../images/burger-icon.svg";
+import productList from "../productList";
 const Products = () => {
   return (
-    <div className="city-filter">
-      {/* ÇiçekSepeti {">"} İstanbul {"> "}
-      <label className="city-filter-lbl">ÇiçekSepetiBreadCrumb</label>
-      <div className="categories-box">
-        <img className="burger-icon" src={burgerIcon}></img>
-        <label className="categories-title">Kategoriler</label>
-      </div>
-      <div className="categories"> */}
-        {/* <div>
-          <button className="categories-button">
-            Tüm Kategoriler
-          </button>
-        </div> */}
-        {/* <nav class="nav nav-pills nav-justified">
-          <a class="nav-item nav-link active" href="#">
-            Active
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Much longer nav link
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Link
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Link
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Link
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Link
-          </a>
-        </nav> */}
-      {/* </div> */}
+    // <div className="city-filter">
+    //    ÇiçekSepeti {">"} İstanbul {"> "}
+    //   <label className="city-filter-lbl">ÇiçekSepetiBreadCrumb</label>
+    //   <div className="categories-box">
+    //     <img className="burger-icon" src={burgerIcon}></img>
+    //     <label className="categories-title">Kategoriler</label>
+    //   </div>
+    //   <div className="categories">
+    //    <div>
+    //       <button className="categories-button">
+    //         Tüm Kategoriler
+    //       </button>
+    //     </div>
+    //  </div>
+    //   <div className="card">
+    //   <img style="product-photo"/>
+    //   <div className="card-body">
+    //     <h5 className="card-title product-title">Card title</h5>
+    //     <p className="card-text product-text">
+    //       Some quick example text to build on the card title and make up the
+    //       bulk of the card's content.
+    //     </p>
+    //     <a href="#" className="btn btn-primary">
+    //       Go somewhere
+    //     </a>
+    //   </div>
+    // </div>
+
+    <div className="all-product-list">
+      {productList.map((e) => (
+        <div className="card">
+          <img
+            src={e.photo}
+            className="card-img-top product-photo"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title product-title">
+              {e.name}
+            </h5>
+            <p className="card-text product-shipping">
+              {e.shipping !== "" ? e.shipping : ""}
+            </p>
+            <p className="card-text product-price">{e.price}</p>
+            <a href="#" className="btn btn-primary">
+              Sepete Ekle
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/productList.js
@@ -0,0 +1,86 @@
+/* eslint-disable import/no-anonymous-default-export */
+// eslint-disable-next-line import/no-anonymous-default-export
+import bag from "./images/bag.png";
+import bracelet from "./images/bracelet.png";
+import chess from "./images/chess.png";
+import chocolate from "./images/chocolate.png";
+import flowers from "./images/flowers.png";
+import parfume from "./images/parfume.png";
+import pinkBear from "./images/pink-bear.png";
+import silverNeedle from "./images/silver-needle.png";
+import smartWatch from "./images/smart-watch.png";
+import stoneTree from "./images/stone-tree.png";
+
+export default [
+  {
+    photo:smartWatch,
+    name: "Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth",
+    shipping: "Ücretsiz Teslimat",
+    price: "399,90 TL",
+    categories:"technology"
+  },
+  {
+    photo: parfume,
+    name: "Calvin Klein Euphoria EDP 100 ml Kadın Parfüm",
+    shipping: "",
+    price: "145,61 TL",
+    categories:"parfume"
+  },
+  {
+    photo: stoneTree,
+    name: "Gold Çizgili Vazoda 2 Dal Orkide Çiçeği",
+    shipping: "Ücretsiz Teslimat",
+    price: "249,90 TL",
+    categories:"homeAccessories"
+  },
+  {
+    photo: bracelet,
+    name: "925 Ayar Gümüş Çeyreklik Bileklik (Y)",
+    shipping: "",
+    price: "329,90 TL",
+    categories:"accessory"
+  },
+  {
+    photo: bag,
+    name: "Sailor SD-3160 Siyah Dağcı Seyahat Sırt Çantası 55 LT Outdoor-Trekking",
+    shipping: "Ücretsiz Teslimat",
+    price: "499,90 TL",
+    categories:"accessory"
+
+  },
+  {
+    photo: chess,
+    name: "Büyük Boy Metal Osmanlı Bizans Satranç Takımı Parlak ve Cev...",
+    shipping: "Ücretsiz Teslimat",
+    price: "19,90 TL",
+    categories:"toy"
+  },
+  {
+    photo: pinkBear,
+    name: "130 Cm Papyonlu Büyük Ayıcı (%100 Yerli)",
+    shipping: "Ücretsiz Teslimat",
+    price: "89,90 TL",
+    categories:"toy"
+  },
+  {
+    photo: silverNeedle,
+    name: "925 Ayar Gümüş Dualı İsimli Nazar İgnesi",
+    shipping: "Ücretsiz Teslimat",
+    price: "29,90 TL",
+    categories:"accessory"
+  },
+  {
+    photo: chocolate,
+    name: "Özel Tatlar Çikolata Kutusu 300 gr",
+    shipping: "Ücretsiz Teslimat",
+    price: "59,90 TL",
+    categories:"gift"
+  },
+  {
+    photo: flowers,
+    name: "Kucak Dolusu Aşk 25 Kırmızı Gül Buketi",
+    shipping: "",
+    price: "199,90 TL",
+    categories:"flowers"
+  },
+];
